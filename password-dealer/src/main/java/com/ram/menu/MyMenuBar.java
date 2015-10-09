fbd68860d0f705001ad8589aa46e305119cb968a
ram naresh singh <ramnaresh2051@gmail.com>
2015-10-08 23:44:15 -0700

    added Menu bar

---
+++ password-dealer/src/main/java/com/ram/menu/MyMenuBar.java
@@ -0,0 +1,62 @@
+package com.ram.menu;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class MyMenuBar extends JFrame{
+	
+	public static JMenuBar creaetMenuBar() {
+		  JMenuBar menubar = new JMenuBar();
+	      ImageIcon icon = new ImageIcon("exit.png");
+
+	      JMenu file = new JMenu("Password Manager");
+	      file.setMnemonic(KeyEvent.VK_F);
+	      
+	      JMenu profileItem = new JMenu("Profile");
+	      profileItem.setMnemonic(KeyEvent.VK_E);
+	      profileItem.setToolTipText("User Profile");
+	      
+	      JMenuItem changePassword = new JMenuItem("Change Password", icon);
+	      changePassword.setMnemonic(KeyEvent.VK_E);
+	      changePassword.setToolTipText("Change User Login Password");
+	      
+	      JMenuItem changeSecurityKey = new JMenuItem("Change SecutrityKey", icon);
+	      changeSecurityKey.setMnemonic(KeyEvent.VK_E);
+	      changeSecurityKey.setToolTipText("Change SecutrityKey");
+	      
+	      JMenuItem changeIdleTimeOut = new JMenuItem("Change Idle TimeOut", icon);
+	      changeIdleTimeOut.setMnemonic(KeyEvent.VK_E);
+	      changeIdleTimeOut.setToolTipText("Change Idle TimeOut");
+	      
+	      profileItem.add(changePassword);
+	      profileItem.add(changeSecurityKey);
+	      profileItem.add(changeIdleTimeOut);
+	      
+	      JMenuItem settings = new JMenuItem("Settings", icon);
+	      settings.setMnemonic(KeyEvent.VK_E);
+	      settings.setToolTipText("User Settings");
+	      
+	      JMenuItem eMenuItem = new JMenuItem("Exit", icon);
+	      eMenuItem.setMnemonic(KeyEvent.VK_E);
+	      eMenuItem.setToolTipText("Exit application");
+	      eMenuItem.addActionListener(new ActionListener() {
+	          @Override
+	          public void actionPerformed(ActionEvent event) {
+	              System.exit(0);
+	          }
+	      });
+	      file.add(profileItem);
+	      file.add(settings);
+	      file.add(eMenuItem);
+	      menubar.add(file);
+		return menubar;
+	}
+
+}
